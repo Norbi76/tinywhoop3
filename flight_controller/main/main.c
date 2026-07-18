d1ba3e756dc0f1d3f0cc33a0c3c8802dc3ce7cfd
nrib <kondora35@gmail.com>
2026-07-18 22:28:58 +0300

    tried gyro compensation, not convinced i m gonna use that

--- flight_controller/main/main.c
+++ flight_controller/main/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "imu_driver.h"
+#include "esp_timer.h"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 static const char *TAG = "FC_MAIN";
+#define ALPHA 0.70
 
 void fc_task(void *args) { //semnatura unui task freeRTOS trebuie sa contine un param de tip void*
     ESP_LOGI(TAG, "Flight controll task started on core %d", xPortGetCoreID());
@@ -15,14 +22,20 @@ void fc_task(void *args) { //semnatura unui task freeRTOS trebuie sa contine un 
         vTaskDelete(NULL);
         return;
     }
+    float roll_offset = 0.0f, pitch_offset = 0.0f;
+    imu_calibrate_acc(&roll_offset, &pitch_offset);
     imu_calibrate_gyro();
 
     const TickType_t xFreq = pdMS_TO_TICKS(1000); // 1ms -> 1kHz
     TickType_t xLastWakeTime = xTaskGetTickCount();
+    // uint64_t last_time = esp_timer_get_time();
 
 
     imu_raw_data_t imu_raw_data;
     imu_physical_data_t imu_physical_data;
+
+    float roll_angle = 0.0f, pitch_angle = 0.0f;
+    // float roll_angle_comp = 0.0f, pitch_angle_comp = 0.0f;
     for (;;) {
         error = imu_read_raw_data(&imu_raw_data);
         if (error != ESP_OK) {
@@ -32,12 +45,25 @@ void fc_task(void *args) { //semnatura unui task freeRTOS trebuie sa contine un 
         }
         imu_convert_raw_to_physical(&imu_raw_data, &imu_physical_data);
 
+        // uint64_t current_time = esp_timer_get_time();
+        // float dt = (current_time - last_time) / 1000000.0f; // convert to seconds
+        // last_time = current_time;
+
+        roll_angle = atan2(imu_physical_data.acc_y_g, imu_physical_data.acc_z_g) * (180.0 / M_PI);
+        pitch_angle = atan2(-imu_physical_data.acc_x_g, 
+                            sqrt(imu_physical_data.acc_y_g * imu_physical_data.acc_y_g + 
+                                 imu_physical_data.acc_z_g * imu_physical_data.acc_z_g)) * (180.0 / M_PI);
+
+        // roll_angle_comp = ALPHA * (roll_angle_comp + imu_physical_data.gyro_x_dps * dt) + (1 - ALPHA) * roll_angle;
+        // pitch_angle_comp = ALPHA * (pitch_angle_comp + imu_physical_data.gyro_y_dps * dt) + (1 - ALPHA) * pitch_angle;
 
         ESP_LOGI(TAG, "IMU Data - Acc: (%.4f, %.4f, %.4f), Gyro: (%.4f, %.4f, %.4f), Temp: %.4f",
         imu_physical_data.acc_x_g, imu_physical_data.acc_y_g, imu_physical_data.acc_z_g,
         imu_physical_data.gyro_x_dps, imu_physical_data.gyro_y_dps, imu_physical_data.gyro_z_dps,
         imu_physical_data.temp_C
         );
+        ESP_LOGI(TAG, "Unghiuri - Roll: %.2f | Pitch: %.2f", roll_angle, pitch_angle);
+        // ESP_LOGI(TAG, "Unghiuri compensare - Roll: %.2f | Pitch: %.2f", roll_angle_comp - roll_offset, pitch_angle_comp - pitch_offset);
                  
         // 2. Calculează PID
         // 3. Scrie comanda PWM către motoarele coreless
